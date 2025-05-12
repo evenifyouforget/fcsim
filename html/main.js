538a76d65396d916cc94bbe10527931aaff19203
evenifyouforget <idiotvtuber@gmail.com>
2025-05-11 21:18:01 -0400

    show self url on save

--- html/main.js
+++ html/main.js
@@ -3,6 +3,13 @@
 const FC_URL = "https://fantasticcontraption.com";
 const SELF_URL = "";
 
+function self_url_full() {
+	// https://stackoverflow.com/a/6257480
+	// ends in /
+	// no query parameters
+	return location.protocol + '//' + location.host + location.pathname;
+}
+
 let play_button  = document.getElementById("play");
 let save_button  = document.getElementById("save");
 let save_menu    = document.getElementById("save_menu");
@@ -388,7 +395,7 @@ function alloc_str(str)
 function on_text(text)
 {
 	console.log(text);
-	design_link.innerHTML = "fcsim.com/?designId=" + text;
+	design_link.innerHTML = self_url_full() + "?designId=" + text;
 	design_link.style.display = "block";
 }
 
