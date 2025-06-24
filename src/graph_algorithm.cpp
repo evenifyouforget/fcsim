47f13e5b887ee4f295e0ea953730e088e66469f3
evenifyouforget <idiotvtuber@gmail.com>
2025-06-24 14:42:57 -0400

    pass memo map by reference

--- src/graph_algorithm.cpp
+++ src/graph_algorithm.cpp
@@ -11,12 +11,12 @@ extern "C" {
 #define CLEAN_COPY(dest, src, attribute_name) (dest).attribute_name = _clean_copy(pointer_map, (src).attribute_name);
 #define CHECK_RETURN(ptr_type, old_obj, new_obj) if(old_obj==nullptr)return nullptr;if(pointer_map.count(old_obj)){return (ptr_type*)pointer_map.at(old_obj);}ptr_type* new_obj = _new<ptr_type>();pointer_map.insert(std::make_pair<void*, void*>(old_obj, new_obj));
 
-attach_node* _clean_copy(std::unordered_map<void*, void*> pointer_map, attach_node* old_obj);
-block* _clean_copy(std::unordered_map<void*, void*> pointer_map, block* old_obj);
-joint* _clean_copy(std::unordered_map<void*, void*> pointer_map, joint* old_obj);
-design* _clean_copy(std::unordered_map<void*, void*> pointer_map, design* old_obj);
+attach_node* _clean_copy(std::unordered_map<void*, void*>& pointer_map, attach_node* old_obj);
+block* _clean_copy(std::unordered_map<void*, void*>& pointer_map, block* old_obj);
+joint* _clean_copy(std::unordered_map<void*, void*>& pointer_map, joint* old_obj);
+design* _clean_copy(std::unordered_map<void*, void*>& pointer_map, design* old_obj);
 
-attach_node* _clean_copy(std::unordered_map<void*, void*> pointer_map, attach_node* old_obj) {
+attach_node* _clean_copy(std::unordered_map<void*, void*>& pointer_map, attach_node* old_obj) {
     CHECK_RETURN(attach_node, old_obj, new_obj);
     CLEAN_COPY(*new_obj, *old_obj, prev);
     CLEAN_COPY(*new_obj, *old_obj, next);
@@ -24,7 +24,7 @@ attach_node* _clean_copy(std::unordered_map<void*, void*> pointer_map, attach_no
     return new_obj;
 }
 
-block* _clean_copy(std::unordered_map<void*, void*> pointer_map, block* old_obj) {
+block* _clean_copy(std::unordered_map<void*, void*>& pointer_map, block* old_obj) {
     CHECK_RETURN(block, old_obj, new_obj);
     CLEAN_COPY(*new_obj, *old_obj, prev);
     CLEAN_COPY(*new_obj, *old_obj, next);
@@ -39,7 +39,7 @@ block* _clean_copy(std::unordered_map<void*, void*> pointer_map, block* old_obj)
     return new_obj;
 }
 
-joint* _clean_copy(std::unordered_map<void*, void*> pointer_map, joint* old_obj) {
+joint* _clean_copy(std::unordered_map<void*, void*>& pointer_map, joint* old_obj) {
     CHECK_RETURN(joint, old_obj, new_obj);
     CLEAN_COPY(*new_obj, *old_obj, prev);
     CLEAN_COPY(*new_obj, *old_obj, next);
@@ -52,7 +52,7 @@ joint* _clean_copy(std::unordered_map<void*, void*> pointer_map, joint* old_obj)
     return new_obj;
 }
 
-design* _clean_copy(std::unordered_map<void*, void*> pointer_map, design* old_obj) {
+design* _clean_copy(std::unordered_map<void*, void*>& pointer_map, design* old_obj) {
     CHECK_RETURN(design, old_obj, new_obj);
     CLEAN_COPY(*new_obj, *old_obj, joints.head);
     CLEAN_COPY(*new_obj, *old_obj, joints.tail);
