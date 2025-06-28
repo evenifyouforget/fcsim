0a79074158ae668ea6176c0a54a3a6d63b68dc39
evenifyouforget <idiotvtuber@gmail.com>
2025-06-27 23:11:09 -0400

    Live memory estimate in bottom bar (web only) (#51)
    * memory estimates
    
    * memory usage display (web only)

--- arch/wasm/include/stdlib.h
+++ arch/wasm/include/stdlib.h
@@ -8,6 +8,10 @@ void *malloc(size_t size);
 void *calloc(size_t nmemb, size_t size);
 void free(void *ptr);
 
+// estimate the total memory used by wasm
+// the actual memory usage will be higher because the JS part is about 20 MB and not included
+size_t total_memory_used_bytes();
+
 #ifdef __cplusplus
 };
 #endif
