1c2b33652972f2a2952f29cfef3a1ecb110f7f57
evenifyouforget <idiotvtuber@gmail.com>
2025-07-15 21:51:50 -0400

    homepage and doc

--- html/main.js
+++ html/main.js
@@ -515,15 +515,16 @@ let module_promise = WebAssembly.instantiateStreaming(
 let design_id = params.get('designId');
 let level_id = params.get('levelId');
 
-// homepage behaviour: default brown
+// homepage behaviour: quantum leap
 if(!design_id && !level_id) {
-    design_id = '12706185';
+    design_id = '12709337';
 
     // show usage
     const notification = document.getElementById("notification");
     if (notification) {
         const helpDiv = document.createElement("div");
 
+        helpDiv.appendChild(chainElement(["p"], "leap variant - design pieces will teleport by +(goal.xy - build.xy) the first time a goal piece enters the goal area"));
         helpDiv.appendChild(chainElement(["p"], "To load levels or designs, use ?levelId or ?designId"));
 		helpDiv.appendChild(chainElement(["p", "b"], "Examples"));
 		helpDiv.appendChild(chainElement(["p"], linkElement(self_url_full() + "?levelId=646726")));
